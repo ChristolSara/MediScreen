a25f41afdc585f4672ed06c474306b73a470428f
SaraaMiss <sara.kaouar.christol@gmail.com>
2023-10-17 08:28:02 +0200

    msDiabete

---
+++ msDiabete/src/test/java/msDiabete/MsDiabeteApplicationTests.java
@@ -0,0 +1,13 @@
+package msDiabete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiabeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
